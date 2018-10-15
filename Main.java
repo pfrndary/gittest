ea1943b9526887999114328753352ed77b90e6eb
pfrndary <email@example.com>
2018-10-15 10:48:45 -0400

    format numbers

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 public class Main {
 	
 	public static void main(String... s) {
-		displayNumber(5.037);
+		displayNumber(5.037, 100005.0, 3.3);
 	}
 	
-	public static void displayNumber(Number number) {
-		System.out.println(number.toString());
+	public static void displayNumber(Number... numbers) {
+		for (Number n : numbers) {
+			System.out.println(String.format("%8.2f", n));
+		}
 	}
 	
 }
