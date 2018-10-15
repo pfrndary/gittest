62cba8428b01467cba4836cf1c788ea71aaaf49a
P <pfremaux@gmail.com>
2018-10-15 14:09:26 -0400

    Ajustements

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 public class Main {
 	
 	public static void main(String... s) {
-		displayNumber(5.037, 100005.0, 3.3);
+		displayNumber(5.037, 100005.0, 3.3, 89.0, 99999999.9);
 	}
 	
 	public static void displayNumber(Number... numbers) {
 		for (Number n : numbers) {
-			System.out.println(String.format("%8.2f", n));
+			System.out.println(String.format("Valeur : \t\t\t%8.2f", n));
 		}
 	}
 	
