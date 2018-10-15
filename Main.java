a8256b4e6dc39e24c0dfb76ee7682c3845bb2091
pfrndary <Conquerors8>
2018-10-15 10:43:07 -0400

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	
+	public static void main(String... s) {
+		displayNumber(5.037);
+	}
+	
+	public static void displayNumber(Number number) {
+		System.out.println(number.toString());
+	}
+	
+}
